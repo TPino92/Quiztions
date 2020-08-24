cb5a5ce1ddcf2ee2fdceb396c538a2721282777d
Trevor Pino <trevorpino@yahoo.com>
2020-08-23 21:41:28 -0600

    added to readme, added endgame state and ability to save score

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Quiztions
+Take a code quiz! Just click the start button and a timer starts. You will be presented with multipule choice questions. When you answer a question incorrectly time is subtracted from the clock, all questions are answered or the timer reaches 0 then the game is over. When the game is over then you can save your name and score.  
+  
+## Screeshot  
+![live website](assets\images\Quiztions.png)  
+
+## Deployed Website  
+[Quiztions!](https://tpino92.github.io/Quiztions/)
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
-var timerEl = document.getElementById('countdown')
-var startButton = document.getElementById('start-btn')
-var nextButton = document.getElementById('next-btn')
-var questionContainerElement = document.getElementById('question-container')
-var questionElement = document.getElementById('question')
-var answerButtonsElement = document.getElementById('answer-buttons')
+var timerEl = document.getElementById('countdown');
+var startButton = document.getElementById('start-btn');
+var nextButton = document.getElementById('next-btn');
+var questionContainerElement = document.getElementById('question-container');
+var questionElement = document.getElementById('question');
+var answerButtonsElement = document.getElementById('answer-buttons');
+var scoreCardElement = document.getElementById('score-card');
+var signUpButton = document.querySelector("#sign-up-btn");
+var userNameSpan = document.querySelector("#name");
+var userHighscoreSpan = document.querySelector("#highscore");
 
 
 let shuffledQuestions, currentQuestionIndex
@@ -57,100 +61,147 @@ function selectAnswer(e) {
   Array.from(answerButtonsElement.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
   })
+  if (!correct) {
+    timeLeft = timeLeft - 5;
+  }
+
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
+    clearInterval(timeInterval);
+    endGame();
   }
 }
+
+//function to end game
+var endGame = function() {
+  startButton.innerText = 'Restart'
+  startButton.classList.remove('hide')
+  questionElement.classList.add('hide')
+  answerButtonsElement.classList.add('hide')
+  scoreCardElement.classList.remove('hide')
+  clearStatusClass(document.body)
+  };
+
 
 function setStatusClass(element, correct) {
   clearStatusClass(element)
   if (correct) {
     element.classList.add('correct')
   } else {
-    element.classList.add('wrong')
+    element.classList.add('wrong') 
   }
 }
 
-function clearStatusClass(element) {
+var clearStatusClass = function(element)  {
   element.classList.remove('correct')
   element.classList.remove('wrong')
 }
 
-const questions = [
+var questions = [
   {
     question: 'Commonly used data types DO NOT include?',
     answers: [
       { text: 'character', correct: false },
       { text: 'booleans', correct: false },
       { text: 'alerts', correct: true},
-      { text: 'numbers', correct: false}
+      { text: 'numbers', correct: false } 
     ]
   },
   {
     question: 'Arrays in JavaScript can be used to store what?',
     answers: [
-      { text: 'numbers & strings', correct: true },
-      { text: 'other arrays', correct: true },
-      { text: 'booleans', correct: true },
-      { text: 'all of the above', correct: true }
+      { text: 'numbers & strings', correct: true},
+      { text: 'other arrays', correct: true},
+      { text: 'booleans', correct: true},
+      { text: 'all of the above', correct: true}
     ]
   },
   {
     question: 'Is web development fun?',
     answers: [
-      { text: 'Kinda', correct: true },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: true },
-      { text: 'IDK', correct: true }
+      { text: 'YES!!!', correct: true},
+      { text: 'Kinda', correct: true},
+      { text: 'IDK', correct: true},
+      { text: 'Um no', correct: true}
     ]
   },
   {
     question: 'The condition of an if/else statement is enclosed in what?',
     answers: [
       { text: 'quotes', correct: false },
-      { text: 'curly brackets', correct: true },
-      { text: 'parentheses', correct: false},
-      { text: 'square brackets', correct: false}
+      { text: 'curly brackets', correct: true},
+      { text: 'parentheses', correct: false },
+      { text: 'square brackets', correct: false }
     ]
   },
   {
-    question: 'String values must be enclosed with __ when being assigned a variable.',
+    question: 'String values must be enclosed with ___ when being assigned a variable.',
     answers: [
       { text: 'comma', correct: false },
       { text: 'curly brackets', correct: false },
-      { text: 'quotes', correct: false},
-      { text: 'parentheses', correct: true}
+      { text: 'quotes', correct: false },
+      { text: 'parentheses', correct: true }
     ]
   },
   {
-    question: 'What is a useful tool in tyhe Development and Debugging process for printing information to the "debugger"?',
+    question: 'What is a useful tool in the Development and Debugging process for printing information to the "debugger"?',
     answers: [
       { text: 'JavaScript', correct: false },
-      { text: 'treminal/bash', correct: false},
-      { text: 'for loops', correct: false},
-      { text: 'console.log', correct: true}
+      { text: 'treminal/bash', correct: false },
+      { text: 'for loops', correct: false },
+      { text: 'console.log', correct: true }
     ]
   }
 ]
 
-function countdown() {
-  var timeLeft = 60;
+var timeLeft = 60;
 
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function() {
-    if (timeLeft > 1) {
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-      timerEl.textContent = '';
-      clearInterval(timeInterval);
-    }
-  }, 1000);
+// Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+var timeInterval = setInterval(function() {
+  if (timeLeft > 1) {
+    timerEl.textContent = timeLeft + ' seconds remaining';
+    timeLeft--;
+  } else if (timeLeft === 1) {
+    timerEl.textContent = timeLeft + ' second remaining';
+    timeLeft--;
+  } else {
+    timerEl.textContent = '';
+    clearInterval(timeInterval);
+    endGame();
+  }
+}, 1000);
 
+//high score
+function renderLastHighscore() {
+  var highscore = localStorage.getItem("highscore");
+  var name = localStorage.getItem("name");
+
+  if (highscore && name === null) {
+    return;
+  }
+
+  userHighscoreSpan.textContent = highscore;
+  userNameSpan.textContent = name;
 }
+
+renderLastHighscore();
+
+
+
+signUpButton.addEventListener('click', newHighscore)
+
+function newHighscore() {
+  var highscore = timeLeft;
+  var name = document.querySelector("#new-name").value;
+
+  if (name === "") {
+    alert("error", "Name cannot be blank");
+  } else {
+    alert("success", "Registered successfully");
+
+    localStorage.setItem("highscore", highscore);
+    localStorage.setItem("name", name);
+    renderLastHighscore();
+  }
+};
